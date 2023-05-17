64230a20e1aa0ab130fddab28e5459fc0d366e78
Deanna Stevens <deanna.sofias@gmail.com>
2023-05-17 17:15:34 -0400

    Add conditionals for plants

---
+++ plants.js
@@ -0,0 +1,8 @@
+var gets_sunshine = true
+var ounces_of_water = 12
+if (gets_sunshine === true && ounces_of_water >= 10){
+    console.log("The plant will thrive!")
+} else {
+    console.log("The plant will die!")
+}
+
